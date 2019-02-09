6c99a9aa7f1c4062f0aab069311191623805fbbd
kobayashitakaki <o.share.thief@gmail.com>
2019-02-09 16:27:01 +0900

    fix scraping

--- scraping/extract_tracks_info.py
+++ scraping/extract_tracks_info.py
@@ -2,23 +2,31 @@ from bs4 import BeautifulSoup as bs
 import requests
 import os
 
-filename = input('input filename: ')
-soup = bs(open(filename), 'html.parser')
-links = soup.find_all("a", class_="audibleTile__audibleHeading")
-hostname = "https://soundcloud.com"
 out_filepath = os.path.dirname(os.path.abspath(__file__)) + '/output.csv'
+input_type = input('input type: ')
+filename = input('input filename: ')
+
+if input_type == 'html':
+    soup = bs(open(filename), 'html.parser')
+    links = soup.find_all("a", class_="audibleTile__audibleHeading")
+    root_url = "https://soundcloud.com"
+    urls = [root_url + link['href'] for link in links]
+
+elif input_type == 'url':
+    urls = [line.strip() for line in open(filename).readlines()]
+
+else:
+    raise ValueError('invalid input type.')
 
 with open(out_filepath, mode='w', encoding='utf-8') as f:
-    for link in links:
-        url = hostname + link['href']
+    for url in urls:
         res = requests.get(url)
-        res.encoding = res.apparent_encoding
-        soup = bs(res.text, 'html.parser')
+        soup = bs(res.content, "lxml")
         page_header_as = soup.find('header').find('h1').find_all('a')
         track_name = page_header_as[0].text
         artist = page_header_as[1].text
         track_id_meta = soup.find('meta', attrs={'property': 'twitter:app:url:iphone'})
         track_id = track_id_meta['content'].split(':')[-1]
-        print(','.join([track_id, link['href'], artist, track_name]))
-        f.write(','.join([track_id, link['href'], artist, track_name]))
+        print(','.join([track_id, url, artist, track_name]))
+        f.write(','.join([track_id, url, artist, track_name]))
         f.write('\n')
