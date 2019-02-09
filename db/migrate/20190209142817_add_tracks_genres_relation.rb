9ae3a0ab996c34af10767b6790911729f3837c9b
kobayashitakaki <o.share.thief@gmail.com>
2019-02-09 23:54:39 +0900

    fix migration

--- db/migrate/20190209142817_add_tracks_genres_relation.rb
+++ db/migrate/20190209142817_add_tracks_genres_relation.rb
@@ -1,8 +1,8 @@
 class AddTracksGenresRelation < ActiveRecord::Migration[5.2]
   def change
     create_table :tracks_genres, id: false do |t|
-      t.belongs_to :tracks, index: true
-      t.belongs_to :genres, index: true
+      t.belongs_to :track, index: true
+      t.belongs_to :genre, index: true
     end
   end
 end
