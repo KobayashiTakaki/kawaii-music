0071c5b9512f2f398ee7fe6058a67b278a30e184
kobayashitakaki <o.share.thief@gmail.com>
2019-02-09 20:13:37 +0900

    create article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_042653) do
+ActiveRecord::Schema.define(version: 2019_02_09_111221) do
+
+  create_table "articles", force: :cascade do |t|
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
