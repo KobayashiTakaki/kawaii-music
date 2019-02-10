ea78b99f034fc7d2dd7f2fc8979c5b0bebf51ad2
kobayashitakaki <o.share.thief@gmail.com>
2019-02-10 21:37:48 +0900

    create tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_100918) do
+ActiveRecord::Schema.define(version: 2019_02_10_123514) do
 
   create_table "articles", force: :cascade do |t|
     t.text "comment"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2019_02_10_100918) do
     t.integer "genre_id"
     t.index ["genre_id"], name: "index_genres_tracks_on_genre_id"
     t.index ["track_id"], name: "index_genres_tracks_on_track_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tracks", force: :cascade do |t|
