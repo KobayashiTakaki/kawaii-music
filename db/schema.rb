ba1d9dda6299e7e4b9b170f1b6e25004470216b9
kobayashitakaki <o.share.thief@gmail.com>
2019-02-10 19:15:35 +0900

    add description column to tracks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_152306) do
+ActiveRecord::Schema.define(version: 2019_02_10_100918) do
 
   create_table "articles", force: :cascade do |t|
     t.text "comment"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_02_09_152306) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
